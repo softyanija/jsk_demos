c3197d078a38a1c794ecb4672f6aeba583a28e4f
softyanija <amakou0614@gmail.com>
2022-08-18 13:34:44 +0900

    success to pub tf of leg

--- assemble_table_pr2/script/single_leg_tf_broadcaster.py
+++ assemble_table_pr2/script/single_leg_tf_broadcaster.py
@@ -8,12 +8,21 @@ import jsk_recognition_msgs.msg
 
 def handle_leg_pose(msg, leg):
     br = tf.TransformBroadcaster()
+    transformer = tf.TransformListener() 
     rospy.loginfo("hoge")
-    br.sendTransform((0, 0, 0),
-                     tf.transformations.quaternion_from_euler(0, 0, 0),
+
+#    (trans, rot) = transformer.lookupTransform("head_mount_kinect_rgb_optical_frame","base_link",rospy.Time(0))
+    
+    br.sendTransform((msg.boxes[0].pose.position.x, msg.boxes[0].pose.position.y, msg.boxes[0].pose.position.z),
+                     (msg.boxes[0].pose.orientation.x, msg.boxes[0].pose.orientation.y, msg.boxes[0].pose.orientation.z, msg.boxes[0].pose.orientation.w),
                      rospy.Time.now(),
                      "leg",
-                     "base_link")
+                     "head_mount_kinect_rgb_optical_frame")
+    # br.sendTransform((msg.boxes[0].pose.position.x, msg.boxes[0].pose.position.y, buf.boxes[0].pose.position.z),
+    #                  (msg.boxes[0].pose.orientation.x, msg.boxes[0].pose.orientation.y, msg.boxes[0].pose.orientation.z, msg.boxes[0].pose.orientation.w),
+    #                  rospy.Time.now(),
+    #                  "leg",
+    #                  "base_link")
 
 if __name__ == '__main__':
     rospy.init_node('leg_tf_broadcaster')

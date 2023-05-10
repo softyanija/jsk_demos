5aa47c8f37d243d995d116793f7b796f1a817544
softyanija <amakou0614@gmail.com>
2023-05-10 18:14:18 +0900

    add pose_1

--- multi_device_view/scripts/prot/camera-calib-motion-prot.py
+++ multi_device_view/scripts/prot/camera-calib-motion-prot.py
@@ -1,6 +1,7 @@
 from skrobot.interfaces.ros import PR2ROSRobotInterface
 import skrobot
 
+
 r = skrobot.models.PR2()
 ri = PR2ROSRobotInterface(r)
 
@@ -24,9 +25,21 @@ link_list = [
     r.r_forearm_roll_link,
     r.r_wrist_flex_link,
     r.r_wrist_roll_link]
+link_list = [r.r_shoulder_pan_link,r.r_shoulder_lift_link,r.r_upper_arm_roll_link,r.r_elbow_flex_link,r.r_forearm_roll_link,r.r_wrist_flex_link,r.r_wrist_roll_link]
 rarm_init_coords = skrobot.coordinates.Coordinates([0.5, -0.3, 0.75], [0, 1.57, 0])
 r.inverse_kinematics(
     rarm_init_coords,
     link_list=link_list,
-    move_target=rarm_init_coords)
-ri.angle_vector(r.angle_vector())
+    move_target=rarm_end_coords)
+ri.angle_vector(r.angle_vector(), 5)
+ri.wait_interpolation()
+
+rarm_calib_pose_0_1 =skrobot.coordinates.Coordinates([0.0, 0.0, 0.0], [0, 0, 0.3])
+
+rarm_calib_pose_1= skrobot.coordinates.base.transform_coords(rarm_init_coords, rarm_calib_pose_0_1)
+r.inverse_kinematics(
+    rarm_calib_pose_1,
+    link_list=link_list,
+    move_target=rarm_end_coords)
+ri.angle_vector(r.angle_vector(), 5)
+ri.wait_interpolation()

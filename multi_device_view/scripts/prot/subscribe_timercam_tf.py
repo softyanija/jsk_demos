f18e66ecd0d45f922ae501941da9378df7953857
softyanija <amakou0614@gmail.com>
2023-05-17 15:12:43 +0900

    add stock_tf func

--- multi_device_view/scripts/prot/subscribe_timercam_tf.py
+++ multi_device_view/scripts/prot/subscribe_timercam_tf.py
@@ -4,28 +4,39 @@ from tf2_msgs.msg import TFMessage
 #write only class definition in this file, and I will make two python program to launch node
 
 class Calculation_timercam_pos:
-    def __init__(self, timercam_name):
+    def __init__(self, timercam_name, timercam_camera_frame="base_link"):
         self.timercam_name = timercam_name
         self.timercam_tf = None
+        self.timercam_tf_stock = []
+        self.timercam_camera_frame = timercam_camera_frame
         self.Rate = 5
         self.subscribe()
+        
         
     def subscribe(self):
         rospy.Subscriber("tf", TFMessage, self.callback)
         rate = rospy.Rate(self.Rate)
         
     def callback(self, data):
-        rospy.loginfo("len of data.transforms is %s",len(data.transforms))
+        #rospy.loginfo("len of data.transforms is %s",len(data.transforms))
+        
         for tf in data.transforms:
-            if tf.header.frame_id == "base_link": #timer_cam1_optical_frame
+            if tf.header.frame_id == self.timercam_camera_frame: #timer_cam1_optical_frame
                 #print("found tf")
                 #print(tf.transform)
                 self.timercam_tf = tf.transform
-                #return tf.transform
-                #tfの入れ物作ってほしいtfが見つかったらそれを返しなかったらNoneを返す
+                
+            else:
+                self.timercam_tf = None
         #print(self.timercam_tf)
 
-
+    def stock_tf(self):
+        if not (self.timercam_tf == None):
+            self.timercam_tf_stock.append(self.timercam_tf)
+            return True
+        else:
+            return False
+        
         
         
         
@@ -49,4 +60,3 @@ class Calculation_timercam_pos:
 
 # if __name__ == '__main__':
 #     get_tf()
-                  

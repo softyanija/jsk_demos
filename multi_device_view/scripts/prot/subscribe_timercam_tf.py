d081d9fad3ec5438419b9ef4d41ab24dea324d0d
softyanija <amakou0614@gmail.com>
2023-05-17 17:00:04 +0900

    make calculation_tf_average func

--- multi_device_view/scripts/prot/subscribe_timercam_tf.py
+++ multi_device_view/scripts/prot/subscribe_timercam_tf.py
@@ -1,7 +1,11 @@
 import rospy
 from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import Transform
+import numpy as np
+from pyquaternion import Quaternion
 
 #write only class definition in this file, and I will make two python program to launch node
+
 
 class Calculation_timercam_pos:
     def __init__(self, timercam_name, timercam_camera_frame="base_link"):
@@ -36,7 +40,45 @@ class Calculation_timercam_pos:
             return True
         else:
             return False
-        
+
+    def calculation_tf_average(self):
+        if self.timercam_tf_stock == []:
+            print("There is no tf in list")
+        else:
+            pose_x = 0
+            pose_y = 0
+            pose_z = 0
+            qlist = []
+            
+            for i in self.timercam_tf_stock:
+                pose_x += i.translation.x
+                pose_y += i.translation.y
+                pose_z += i.translation.z
+
+                q_buf = Quaternion(i.rotation.w, i.rotation.x, i.rotation.y, i.rotation.z)
+                qlist.append(q_buf)
+                
+
+            pose_x = pose_x / len(self.timercam_tf_stock)
+            pose_y = pose_y / len(self.timercam_tf_stock)
+            pose_z = pose_z / len(self.timercam_tf_stock)
+                
+            x = np.array([q.elements for q in qlist])
+            m = x.T @ x
+            w,v = np.linalg.eig(m)
+            #return Quaternion(v[:, np.argmax(w)])
+            q_average = Quaternion(v[:, np.argmax(w)])
+
+            tf_average = Transform()
+            tf_average.translation.x = pose_x
+            tf_average.translation.y = pose_y
+            tf_average.translation.z = pose_z
+            tf_average.rotation.x = q_average.x
+            tf_average.rotation.y = q_average.y
+            tf_average.rotation.z = q_average.z
+            tf_average.rotation.w = q_average.w
+
+            return tf_average
         
         
         

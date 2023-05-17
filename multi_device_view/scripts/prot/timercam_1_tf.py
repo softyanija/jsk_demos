b076177540834eb6770a286f9c88c229d03a3c0c
softyanija <amakou0614@gmail.com>
2023-05-17 15:13:04 +0900

    add test mode

--- multi_device_view/scripts/prot/timercam_1_tf.py
+++ multi_device_view/scripts/prot/timercam_1_tf.py
@@ -1,5 +1,15 @@
 import rospy
 from subscribe_timercam_tf import Calculation_timercam_pos
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", action="store_true")
+args = parser.parse_args()
+
+if args.test:
+    frame_name = "base_link"
+else:
+    frame_name = "timer_cam1_optical_frame"
 
 rospy.init_node("sucscribe_timercam_1_tf")
-Calculation_timercam_pos_1 = Calculation_timercam_pos(timercam_name = "timer_cam1")
+calculation_timercam_pos_1 = Calculation_timercam_pos(timercam_name = "timer_cam1", timercam_camera_frame = frame_name)

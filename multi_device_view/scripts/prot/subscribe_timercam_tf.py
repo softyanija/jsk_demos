1783b239cd51100960b86505bf6b7270c0607fdb
softyanija <amakou0614@gmail.com>
2023-05-18 12:04:40 +0900

    arange

--- multi_device_view/scripts/prot/subscribe_timercam_tf.py
+++ multi_device_view/scripts/prot/subscribe_timercam_tf.py
@@ -15,24 +15,24 @@ class Calculation_timercam_pos:
         self.timercam_camera_frame = timercam_camera_frame
         self.Rate = 5
         self.subscribe()
-        
-        
+
     def subscribe(self):
         rospy.Subscriber("tf", TFMessage, self.callback)
         rate = rospy.Rate(self.Rate)
-        
+        rate.sleep()
+
     def callback(self, data):
-        #rospy.loginfo("len of data.transforms is %s",len(data.transforms))
+        # rospy.loginfo("len of data.transforms is %s",len(data.transforms))
         
         for tf in data.transforms:
-            if tf.header.frame_id == self.timercam_camera_frame: #timer_cam1_optical_frame
-                #print("found tf")
-                #print(tf.transform)
+            if tf.header.frame_id == self.timercam_camera_frame:  # timer_cam1_optical_frame
+                # print("found tf")
+                # print(tf.transform)
                 self.timercam_tf = tf.transform
                 
             else:
                 self.timercam_tf = None
-        #print(self.timercam_tf)
+        # print(self.timercam_tf)
 
     def stock_tf(self):
         if not (self.timercam_tf == None):
@@ -57,16 +57,14 @@ class Calculation_timercam_pos:
 
                 q_buf = Quaternion(i.rotation.w, i.rotation.x, i.rotation.y, i.rotation.z)
                 qlist.append(q_buf)
-                
 
             pose_x = pose_x / len(self.timercam_tf_stock)
             pose_y = pose_y / len(self.timercam_tf_stock)
             pose_z = pose_z / len(self.timercam_tf_stock)
-                
+
             x = np.array([q.elements for q in qlist])
             m = x.T @ x
-            w,v = np.linalg.eig(m)
-            #return Quaternion(v[:, np.argmax(w)])
+            w, v = np.linalg.eig(m)
             q_average = Quaternion(v[:, np.argmax(w)])
 
             tf_average = Transform()
@@ -79,26 +77,3 @@ class Calculation_timercam_pos:
             tf_average.rotation.w = q_average.w
 
             return tf_average
-        
-        
-        
-
-
-
-# def callback(data):
-#     rospy.loginfo("len of data.transforms is %s",len(data.transforms))
-#     for tf in data.transforms:
-#        if tf.header.frame_id == "base_link":
-#            print("found base_link")
-#            print(tf.transform)
-#            return tf.transform
-
-# def get_tf():
-#     rospy.init_node("tf_listener", anonymous=True)
-#     rospy.Subscriber("tf", TFMessage, callback)
-#     rate = rospy.Rate(5)
-#     while not rospy.is_shutdown():
-#         rate.sleep()
-
-# if __name__ == '__main__':
-#     get_tf()

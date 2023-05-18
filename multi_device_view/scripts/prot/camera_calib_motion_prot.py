158dc032ce23ce5d75f650cd7f50a00c2d4367fd
softyanija <amakou0614@gmail.com>
2023-05-18 12:03:56 +0900

    add stock_tf

--- multi_device_view/scripts/prot/camera_calib_motion_prot.py
+++ multi_device_view/scripts/prot/camera_calib_motion_prot.py
@@ -1,17 +1,25 @@
 from skrobot.interfaces.ros import PR2ROSRobotInterface
 import skrobot
 import camera_calib_params
+from subscribe_timercam_tf import Calculation_timercam_pos
+import argparse
+import rospy
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--test", action="store_true")
+args = parser.parse_args()
+
+if args.test:
+    frame_name = "base_link"
+else:
+    frame_name = "timercam_1_optical_frame"
+    
+rospy.init_node("sucscribe_timercam_1_tf")
+calculation_timercam_pos_1 = Calculation_timercam_pos(timercam_name = "timer_cam1", timercam_camera_frame = frame_name)
 
 r = skrobot.models.PR2()
 ri = PR2ROSRobotInterface(r)
-
-viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-viewer.add(r)
-viewer.show()
-
-# ri.angle_vector(r.reset_pose())
-# ri.angle_vector(r.reset_pose(), 5)
-
 rarm_link_list = [
     r.r_shoulder_pan_link,
     r.r_shoulder_lift_link,
@@ -20,6 +28,13 @@ rarm_link_list = [
     r.r_forearm_roll_link, 
     r.r_wrist_flex_link,
     r.r_wrist_roll_link]
+
+viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+viewer.add(r)
+viewer.show()
+
+# ri.angle_vector(r.reset_pose())
+# ri.angle_vector(r.reset_pose(), 5)
 
 ri.angle_vector(r.reset_manip_pose(), 5)
 rarm_end_coords = skrobot.coordinates.CascadedCoords(
@@ -41,6 +56,9 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 1")
+    time.sleep(1)
 
 rarm_calib_pose_2 = skrobot.coordinates.base.transform_coords(camera_calib_params.rarm_init_coords, camera_calib_params.rarm_calib_pose_0_2)
 r.inverse_kinematics(
@@ -49,6 +67,9 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 2")
+    time.sleep(1)
 
 rarm_calib_pose_3 = skrobot.coordinates.base.transform_coords(camera_calib_params.rarm_init_coords, camera_calib_params.rarm_calib_pose_0_3)
 r.inverse_kinematics(
@@ -57,6 +78,9 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 3")
+    time.sleep(1)
 
 rarm_calib_pose_4 = skrobot.coordinates.base.transform_coords(camera_calib_params.rarm_init_coords, camera_calib_params.rarm_calib_pose_0_4)
 r.inverse_kinematics(
@@ -65,6 +89,9 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 4")
+    time.sleep(1)
 
 rarm_calib_pose_5 = skrobot.coordinates.base.transform_coords(camera_calib_params.rarm_init_coords, camera_calib_params.rarm_calib_pose_0_5)
 r.inverse_kinematics(
@@ -73,6 +100,9 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 5")
+    time.sleep(1)
 
 rarm_calib_pose_6 = skrobot.coordinates.base.transform_coords(camera_calib_params.rarm_init_coords, camera_calib_params.rarm_calib_pose_0_6)
 r.inverse_kinematics(
@@ -81,3 +111,8 @@ r.inverse_kinematics(
     move_target=rarm_end_coords)
 ri.angle_vector(r.angle_vector(), 5)
 ri.wait_interpolation()
+while (not calculation_timercam_pos_1.stock_tf()):
+    print("now getting timercam tf at pose 6")
+    time.sleep(1)
+
+print(calculation_timercam_pos_1.calculation_tf_average())

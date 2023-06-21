13604522026ba88384b1d8fee49f6cdcbaa55612
softyanija <amakou0614@gmail.com>
2023-06-21 17:38:53 +0900

    improve export_image_from_rosbag.py to mkdir output_dir if it doesn't exist

--- multi_device_view/scripts/recognition/screw_hole/export_image_from_rosbag.py
+++ multi_device_view/scripts/recognition/screw_hole/export_image_from_rosbag.py
@@ -7,10 +7,13 @@ from sensor_msgs.msg import Image
 
 timestamps = []
 images = []
-count = 0
+
 image_topic = "/timercam_1/timer_cam_image/image_rect_color"
-output_dir = "./images"
-bag_file = "/home/amabe/rosbag/0620_recog_test.bag"
+output_dir = "./images_screw_clip"
+bag_file = "/home/amabe/rosbag/0621_screw.bag"
+
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
 
 for topic, msg, t in  rosbag.Bag(bag_file).read_messages():
     if topic == image_topic:
@@ -18,4 +21,4 @@ for topic, msg, t in  rosbag.Bag(bag_file).read_messages():
         images.append( CvBridge().imgmsg_to_cv2(msg, "8UC3") )
 
 for i in range(len(images)):
-    cv2.imwrite(os.path.join(output_dir, "screw_hole_%06i.png" % i), images[i])
+    cv2.imwrite(os.path.join(output_dir, "screw_%04i.png" % i), images[i])

f0bf30389a87ad5f1c0db8323dfd1f8f03df82c7
softyanija <amakou0614@gmail.com>
2023-07-10 22:02:16 +0900

    send tf from-camera-to-apriltag

--- multi_device_view/scripts/recognition/d405/set_d405_tf.py
+++ multi_device_view/scripts/recognition/d405/set_d405_tf.py
@@ -1,7 +1,11 @@
 import rospy
 import tf
 import tf2_ros
+import skrobot
+
+from skrobot.coordinates.quaternion import Quaternion
 from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import TransformStamped
 from geometry_msgs.msg import *
 from dynamic_tf_publisher.srv import SetDynamicTF
 
@@ -16,11 +20,53 @@ class Set_d405_tf:
         tf_listener = tf2_ros.TransformListener(tf_buffer)
 
         try:
-            trans = tf_buffer.lookup_transform("base_link", "camera_link", rospy.Time(), rospy.Duration(3))
+            #trans = tf_buffer.lookup_transform("base_link", "camera_link", rospy.Time(), rospy.Duration(3))
+            b2g = tf_buffer.lookup_transform("base_link", "r_gripper_front", rospy.Time(), rospy.Duration(3))
+            g2c = tf_buffer.lookup_transform("r_gripper_front_apriltag", "camera_link", rospy.Time(), rospy.Duration(3))
+            base_to_gripper = skrobot.coordinates.Coordinates([b2g.transform.translation.x,
+                                                               b2g.transform.translation.y,
+                                                               b2g.transform.translation.z],
+                                                              [b2g.transform.rotation.w,
+                                                               b2g.transform.rotation.x,
+                                                               b2g.transform.rotation.y,
+                                                               b2g.transform.rotation.z]
+            )
+            gripper_to_camera = skrobot.coordinates.Coordinates([g2c.transform.translation.x,
+                                                                 g2c.transform.translation.y,
+                                                                 g2c.transform.translation.z],
+                                                                [g2c.transform.rotation.w,
+                                                                 g2c.transform.rotation.x,
+                                                                 g2c.transform.rotation.y,
+                                                                 g2c.transform.rotation.z])
+            
+            # base_to_gripper.translation = [b2g.transform.translation.x,
+            #                                b2g.transform.translation.y,
+            #                                b2g.transform.translation.z]
+            # base_to_gripper.quaternion = [b2g.transform.rotation.w,
+            #                               b2g.transform.rotation.x,
+            #                               b2g.transform.rotation.y,
+            #                               b2g.transform.rotation.z]
+            # gripper_to_camera.translation = [g2c.transform.translation.x,
+            #                                  g2c.transform.translation.y,
+            #                                  g2c.transform.translation.z]
+            # gripper_to_camera.quaternion = [g2c.transform.rotation.w,
+            #                                 g2c.transform.rotation.x,
+            #                                 g2c.transform.rotation.y,
+            #                                 g2c.transform.rotation.z]
+
+            base_to_camera = base_to_gripper.copy_worldcoords().transform(gripper_to_camera)
+            
             new_tf = TransformStamped()
             new_tf.header.frame_id = "base_link"
             new_tf.child_frame_id = "camera_link"
-            new_tf.transform = trans.transform
+            new_tf.transform.translation.x = base_to_camera.translation[0]
+            new_tf.transform.translation.y = base_to_camera.translation[1]
+            new_tf.transform.translation.z = base_to_camera.translation[2]
+            new_tf.transform.rotation.x = base_to_camera.quaternion[1]
+            new_tf.transform.rotation.y = base_to_camera.quaternion[2]
+            new_tf.transform.rotation.z = base_to_camera.quaternion[3]
+            new_tf.transform.rotation.w = base_to_camera.quaternion[0]
+            
             self.estimated_tf = new_tf
 
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):

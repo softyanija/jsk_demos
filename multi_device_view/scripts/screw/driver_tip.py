6f1223f6c1259a54f75bffc3742d45924ad5b2f1
softyanija <amakou0614@gmail.com>
2023-07-11 20:17:54 +0900

    pub driver_tip_frame tf

--- multi_device_view/scripts/screw/driver_tip.py
+++ multi_device_view/scripts/screw/driver_tip.py
@@ -5,9 +5,15 @@ import tf
 import tf2_ros
 import time
 
-
-from geometry_msgs.msg import PoseStamped, PoseArray, WrenchStamped, Point
+from skrobot.coordinates import CascadedCoords
+from skrobot.coordinates import Coordinates
+from skrobot.coordinates.quaternion import Quaternion
+from skrobot.interfaces.ros import PR2ROSRobotInterface
+from skrobot.interfaces.ros.tf_utils import tf_pose_to_coords
+from skrobot.interfaces.ros.tf_utils import geometry_pose_to_coords
+from geometry_msgs.msg import PoseStamped, PoseArray, WrenchStamped, Point, TransformStamped
 from visualization_msgs.msg import Marker
+from dynamic_tf_publisher.srv import SetDynamicTF
 
 class Driver():
     def __init__(self):
@@ -15,25 +21,26 @@ class Driver():
         self.start_point = None
         self.end_point = None
         self.tip_frame = None
+        self.tip_length = 0.0065
+        self.tool_frame = None 
         self.rate = 5
-        #self.subscribe
+        self.tf_hz = 10
 
-    # def subscribe(self):
-    #     rospy.Subscriber("/driver/line_segment_detector/debug/line_marker", Marker, self.cb)
-    #     rate = rospy.Rate(self.rate)
-    #     rate.sleep()
+    def get_tool_frame(self):
+        tf_buffer = tf2_ros.Buffer()
+        tf_listener = tf2_ros.TransformListener(tf_buffer)
+        self.tool_frame = tf_buffer.lookup_transform("camera_color_optical_frame", "r_gripper_tool_frame", rospy.Time(), rospy.Duration(3))
 
     def select_line(self):
         if len(self.msg.points) == 0:
             return
         center_list = []
-        tf_buffer = tf2_ros.Buffer()
-        tf_listener = tf2_ros.TransformListener(tf_buffer)
-        tool_frame = tf_buffer.lookup_transform("camera_color_optical_frame", "r_gripper_tool_frame", rospy.Time(), rospy.Duration(3))
+        self.get_tool_frame()
+        
         tool_frame_center = Point()
-        tool_frame_center.x = tool_frame.transform.translation.x
-        tool_frame_center.y = tool_frame.transform.translation.y
-        tool_frame_center.z = tool_frame.transform.translation.z
+        tool_frame_center.x = self.tool_frame.transform.translation.x
+        tool_frame_center.y = self.tool_frame.transform.translation.y
+        tool_frame_center.z = self.tool_frame.transform.translation.z
 
         for i in range(int(len(self.msg.points)/2)):
             center_buf = Point()
@@ -48,7 +55,7 @@ class Driver():
             range_buf = (tool_frame_center.x - center_list[i].x)**2 + (tool_frame_center.y - center_list[i].y)**2 + (tool_frame_center.z - center_list[i].z)**2
             if min_range > range_buf:
                 closest_i = i
-                min_range = range
+                min_range = range_buf
 
         edge_1 = self.msg.points[2*closest_i]
         edge_2 = self.msg.points[2*closest_i + 1]
@@ -62,8 +69,43 @@ class Driver():
             self.start = self.msg.points[2*closest_i + 1]
             self.end = self.msg.points[2*closest_i]
         
+    def calc_tip(self):
+        start = skrobot.coordinates.Coordinates([self.start.x, self.start.y, self.start.z],
+                                                [1, 0, 0, 0])
+        end = skrobot.coordinates.Coordinates([self.end.x, self.end.y, self.end.z],
+                                              [1, 0, 0, 0])
+        direction = end.translation - start.translation
+        normalized_direction = direction / np.linalg.norm(direction)
+        tip_translation = end.translation + self.tip_length * normalized_direction
         
+        self.get_tool_frame()
+        axis_convert = skrobot.coordinates.Coordinates([0, 0, 0], [0.5, 0.5, 0.5, 0.5])
+        tip_frame_buf = skrobot.coordinates.Coordinates(tip_translation,
+                                                         [self.tool_frame.transform.rotation.w,
+                                                          self.tool_frame.transform.rotation.x,
+                                                          self.tool_frame.transform.rotation.y,
+                                                          self.tool_frame.transform.rotation.z])
+        self.tip_frame = tip_frame_buf.copy_worldcoords().transform(axis_convert)
+
+    def pub_tip_frame_tf(self):
+        tip_tf = TransformStamped()
+        tip_tf.header.frame_id = "camera_color_optical_frame"
+        tip_tf.child_frame_id = "driver_tip_frame"
+        tip_tf.transform.translation.x = self.tip_frame.translation[0]
+        tip_tf.transform.translation.y = self.tip_frame.translation[1]
+        tip_tf.transform.translation.z = self.tip_frame.translation[2]
+        tip_tf.transform.rotation.x = self.tip_frame.quaternion[1]
+        tip_tf.transform.rotation.y = self.tip_frame.quaternion[2]
+        tip_tf.transform.rotation.z = self.tip_frame.quaternion[3]
+        tip_tf.transform.rotation.w = self.tip_frame.quaternion[0]
         
+        rospy.wait_for_service("/set_dynamic_tf")
+        try:
+            client = rospy.ServiceProxy("/set_dynamic_tf", SetDynamicTF)
+            res = client(self.tf_hz, tip_tf)
+            return
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
 
     def cb(self, msg):
         self.msg = msg

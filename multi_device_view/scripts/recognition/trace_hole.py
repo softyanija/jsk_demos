8d7bcdf0b67b11e1d244514f95ab088e905ecc1c
softyanija <amakou0614@gmail.com>
2023-11-13 18:26:24 +0900

    add rect limit

--- multi_device_view/scripts/recognition/trace_hole.py
+++ multi_device_view/scripts/recognition/trace_hole.py
@@ -26,8 +26,7 @@ while True:
     equ_img = np.hstack((gray_img, equ_hist))
     equ_height, equ_width = equ_img.shape
     equ_img_clip = equ_img[:, equ_width//2:equ_width]
-    _, threshold_image = cv2.threshold(equ_img_clip, 90, 255, cv2.THRESH_BINARY)
-    #threshold_image = cv2.adaptiveThreshold(equ_img_clip, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    _, threshold_image = cv2.threshold(equ_img_clip, 130, 255, cv2.THRESH_BINARY)
 
     if stage == 0:
         key = cv2.waitKey(1)
@@ -38,8 +37,7 @@ while True:
             stage = 1
             print("press Space for set after image")
 
-    if stage == 1:
-        
+    if stage == 1:        
         key = cv2.waitKey(1)
         if key == ord(' '):
             gray_img_after = gray_img.copy()
@@ -55,7 +53,7 @@ while True:
                 if (size_buff < width * height) and (y < gray_img_before.shape[1]//2):
                     size_buff = width * height
                     index_buff = i
-            #pdb.set_trace()
+
             if index_buff is not None:
                 x, y, width, height = cv2.boundingRect(diff_contours[index_buff])
                 roi_param = ()
@@ -66,8 +64,6 @@ while True:
                 stage = 2
             else:
                 print("can't find enough diff, try again")
-
-    #_, threshold_image = cv2.threshold(equ_img_clip, 35, 255, cv2.THRESH_BINARY)
 
     if stage == 2:
         diff  = cv2.absdiff(gray_img_before, gray_img)
@@ -87,65 +83,54 @@ while True:
         if index_buff is not None:
             x, y, width, height = cv2.boundingRect(diff_contours[index_buff])
             roi_param = ()
-        mask_roi = np.zeros_like(threshold_image)
-        mask_roi = cv2.rectangle(mask_roi, (x, y), (x + width, y + height), 255, cv2.FILLED)
-        mask = cv2.bitwise_and(diff_thresholded, mask_roi)
-        masked_image = cv2.bitwise_and(threshold_image, mask_roi)
-        #masked_image = cv2.bitwise_and(masked_image, mask_roi)
-        #mask_image = cv2.bitwise_and(diff_thresholded, mask_roi)
+            mask_roi = np.zeros_like(threshold_image)
+            mask_roi = cv2.rectangle(mask_roi, (x, y), (x + width, y + height), 255, cv2.FILLED)
+            mask = cv2.bitwise_and(diff_thresholded, mask_roi)
+            masked_image = cv2.bitwise_and(threshold_image, mask_roi)
+
+            
+            cv2.imshow("threshold_image", threshold_image)
+            cv2.imshow("mask_roi", mask_roi)
+            cv2.imshow("masked_image", masked_image)
+            cv2.imshow("diff_image", diff)
+
+            ellipse_contours, _ = cv2.findContours(masked_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
-        #cv2.imshow("diff_image", diff)
-        #cv2.imshow("diff_th", diff_thresholded)
-        cv2.imshow("threshold_image", threshold_image)
-        cv2.imshow("mask_roi", mask_roi)
-        cv2.imshow("masked_image", masked_image)
-        
-        #ellipse_contours, _ = cv2.findContours(threshold_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        ellipse_contours, _ = cv2.findContours(masked_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
-        ellipse_drawed_image = frame.copy()
-        ellipse_list = []
-        for i, cnt in enumerate(ellipse_contours):
-            if len(cnt) >= 5: 
-                ellipse = cv2.fitEllipse(cnt)
-                if (not math.isnan(ellipse[0][0])) and (not math.isnan(ellipse[0][1])) and (not math.isnan(ellipse[1][0])) and (not math.isnan(ellipse[1][1])):
-                    ellipse_list.append(ellipse)
-                    cx = int(ellipse[0][0])
-                    cy = int(ellipse[0][1])
-                    h = int(ellipse[1][0])
-                    w = int(ellipse[1][1])
-                    if h * w > 30:
-                        ellipse_drawed_image  = cv2.ellipse(ellipse_drawed_image, ellipse, (255,0,0),2)
-           
-                #ellipse_list = [l[0:2] for l in ellipse_list]
+            ellipse_limit_image = frame.copy()
+            ellipse_list = []
+            x_range_min = 30
+            x_range_max = 130
+            cv2.rectangle(ellipse_limit_image, (x + x_range_min, y), (x + x_range_max, y + height), (255, 0, 0), 4, cv2.LINE_AA)
 
-                ellipse_param = np.array([l[0:2] for l in ellipse_list])
-                new_centers = []
+            ellipse_max_index = None
+            ellipse_max_size = 0
+            for i, cnt in enumerate(ellipse_contours):
+                if len(cnt) >= 5: 
+                    ellipse = cv2.fitEllipse(cnt)
+                    if (not math.isnan(ellipse[0][0])) and (not math.isnan(ellipse[0][1])) and (not math.isnan(ellipse[1][0])) and (not math.isnan(ellipse[1][1])):
+                        ellipse_list.append(ellipse)
+                        cx = int(ellipse[0][0])
+                        cy = int(ellipse[0][1])
+                        h = int(ellipse[1][0])
+                        w = int(ellipse[1][1])
+                        if (h * w > 300) and (h * w < 2000) and (cx > x + x_range_min) and(cx < x + x_range_max):
+                            if h * w > ellipse_max_size:
+                                ellipse_max_size = h * w
+                                ellipse_max_index = i
+                            #ellipse_limit_image  = cv2.ellipse(ellipse_limit_image, ellipse, (255,0,0),2)
+                            print(ellipse)
 
-            # if ellipse_list != []:
-            #     ellipse_param = np.array([l[0:2] for l in ellipse_list])
-            #     for i, param in enumerate(ellipse_param[:, 0:2]):
-            #         center = (param[0][0], param[0][1])
+                    ellipse_param = np.array([l[0:2] for l in ellipse_list])
+                    new_centers = []
+                if ellipse_max_index is not None:
+                    ellipse = cv2.fitEllipse(ellipse_contours[ellipse_max_index])
+                    ellipse_limit_image  = cv2.ellipse(ellipse_limit_image, ellipse, (255,0,0), 2)
+                    
+            cv2.imshow("ellipse", ellipse_limit_image)
+            cv2.imshow("before", gray_img_before)
+            cv2.imshow("after", gray_img_after)
+            cv2.imshow("roi", roi_image)
 
-            #         if prev_centers:
-            #             # make here
-            #             closest_center = min(prev_centers, key=lambda c: np.linalg.norm(np.array(c) - np.array(center)))
-
-            #             if np.linalg.norm(np.array(closest_center) - np.array(center)) < 10:
-            #                 cv2.line(frame, (int(closest_center[0]), int(closest_center[1])), (int(center[0]), int(center[1])), (0, 255, 0), 2)
-            #                 cv2.ellipse(frame, ellipse_list[i], (0, 255, 0), 2)
-            #         #new_centers.append(center)
-            #     new_centers.append(center)
-            #     prev_centers = new_centers
-                # if meet the requirements
-
-                cv2.imshow("ellipse", ellipse_drawed_image)
-                cv2.imshow("before", gray_img_before)
-                cv2.imshow("after", gray_img_after)
-                cv2.imshow("roi", roi_image)
-                   
-
-    #cv2.imshow("trace", frame)
     cv2.imshow("equalize", equ_img_clip)
     cv2.imshow("thresholded", threshold_image)
         

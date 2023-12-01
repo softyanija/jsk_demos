feb948b8cea59e3078af636fe93197a6317540fa
softyanija <amakou0614@gmail.com>
2023-12-01 16:42:28 +0900

    make servo_gear.py to use depth and color filter

--- multi_device_view/scripts/recognition/servo_gear.py
+++ multi_device_view/scripts/recognition/servo_gear.py
@@ -30,9 +30,11 @@ class ServoGear():
         
         self.camera = camera
         self.recognition_object = "servo_gear"
-        self.debug_mode = debug_mode
-        self.sub_image = None
-        self.gray_img = None
+        self.debug_mode = False
+        self.sub_color_image = None
+        self.sub_depth_image = None
+        self.range_th_min = 400
+        self.range_th_max = 800
         self.debug_image_raw = None
         self.bridge = CvBridge()
         self.diff_before = None
@@ -41,76 +43,31 @@ class ServoGear():
         self.background_image = None
         self.background_is_set = False
         self.pub_debug_image_raw = rospy.Publisher(self.camera + "/" + self.recognition_object + "/debug_image_raw", Image, queue_size=10)
-        self.pub_equ_image = rospy.Publisher(self.camera + "/" + self.recognition_object + "/equalizehist_image", Image, queue_size=10)
-        self.pub_threshold_image = rospy.Publisher(self.camera + "/" + self.recognition_object + "/threshold_image", Image, queue_size=10)
-        # self.pub_ellipse_image = rospy.Publisher(self.camera + "/upper_arm_hole/ellipse_image", Image, queue_size=10)
-        # self.pub_background_image = rospy.Publisher(self.camera + "/upper_arm_hole/background_image", Image, queue_size=10)
-        # self.pub_diff_image = rospy.Publisher(self.camera + "/upper_arm_hole/diff_image", Image, queue_size=10)
-        # self.pub_roi_image = rospy.Publisher(self.camera + "/upper_arm_hole/roi_image", Image, queue_size=10)
-        # self.pub_masked_image = rospy.Publisher(self.camera + "/upper_arm_hole/masked_image", Image, queue_size=10)
-        # self.pub_ellipse = rospy.Publisher(self.camera + "/upper_arm_hole/ellipse", RotatedRectStamped, queue_size=10)
-
-        # self.service_name = "/" + camera + "/set_background"
-        # self.service = rospy.Service(self.service_name, Empty, self.set_backgound_image)
+        self.pub_debug_depth_image = rospy.Publisher(self.camera + "/" + self.recognition_object + "/debug_depth_image", Image, queue_size=10)
+        self.pub_mask_depth = rospy.Publisher(self.camera + "/" + self.recognition_object + "/mask_depth", Image, queue_size=10)
+        self.pub_mask_depth_test = rospy.Publisher(self.camera + "/" + self.recognition_object + "/mask_depth_test", Image, queue_size=10)
 
         self.subscribe()
+
+        rospy.set_param(self.camera + "/" + self.recognition_object + "/" + "range_th_min", 400)
+        rospy.set_param(self.camera + "/" + self.recognition_object + "/" + "range_th_max", 800)
         
     def subscribe(self):
-        sub_image = rospy.Subscriber(self.camera + "/color/image_rect_color", Image, self.cb)
-
-    def set_backgound_image(self, req):
-        rospy.loginfo("set background image")
-        self.background_image = self.gray_img.copy()
-        self.background_is_set = True
-        return EmptyResponse()
+        sub_color_image = rospy.Subscriber(self.camera + "/color/image_rect_color", Image, self.color_cb)
+        sub_depth_image = rospy.Subscriber(self.camera + "/depth/image_rect_raw", Image, self.depth_cb)
         
-    def cb(self, image):
-        self.sub_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
+    def color_cb(self, image):
+        self.sub_color_image = self.bridge.imgmsg_to_cv2(image, "bgr8")
         self.header = image.header
 
-    def get_before_image(self):
-        while self.diff_before is None:
-            if (self.sub_image is not None):
-                gray_img = cv2.cvtColor(self.sub_image, cv2.COLOR_BGR2GRAY)
-                gray_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-        
-                equ_hist = cv2.equalizeHist(gray_img)
-                equ_img = np.hstack((gray_img, equ_hist))
-                equ_height, equ_width = equ_img.shape
-                equ_img_clip = equ_img[:, equ_width//2:equ_width]
-
-                self.diff_before = equ_img_clip
-
-                
-    def get_after_image(self):
-        while self.diff_after is None:
-            if (self.sub_image is not None):
-                gray_img = cv2.cvtColor(self.sub_image, cv2.COLOR_BGR2GRAY)
-                gray_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-        
-                equ_hist = cv2.equalizeHist(gray_img)
-                equ_img = np.hstack((gray_img, equ_hist))
-                equ_height, equ_width = equ_img.shape
-                equ_img_clip = equ_img[:, equ_width//2:equ_width]
-
-                self.diff_after = equ_img_clip
-
-                
-    def set_part_roi(self):
-        diff = cv2.absdiff(self.diff_before, self.diff_after)
-        cv2.imwrite("/home/amabe/rosbag/diff.png", diff)
-        ret, diff = cv2.threshold(diff, 50, 255, cv2.THRESH_BINARY)
-        cv2.imwrite("/home/amabe/rosbag/diff_th.png", diff)
-
+    def depth_cb(self, image):
+        self.sub_depth_image = self.bridge.imgmsg_to_cv2(image, "16UC1")
+        self.header = image.header
         
     def run(self):
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(5)
 
         rospy.loginfo("start to recognize servo gear")
-
-        # rospy.loginfo("Waiting for service call...")
-        # rospy.wait_for_service(self.service_name)
-        # rospy.loginfo("Service is now available")
         
         while not rospy.is_shutdown():
             try:
@@ -120,23 +77,31 @@ class ServoGear():
                 rospy.logwarn("cought {}".format(e))
                 pass
             
-            if self.sub_image is not None:
+            if (self.sub_color_image is not None) and (self.sub_depth_image is not None):
                 #pdb.set_trace()
-                gray_img = cv2.cvtColor(self.sub_image, cv2.COLOR_BGR2GRAY)
-                gray_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
-                self.gray_img = gray_img.copy()
+                self.range_th_min = rospy.get_param(self.camera + "/" + self.recognition_object + "/" + "range_th_min")
+                self.range_th_max = rospy.get_param(self.camera + "/" + self.recognition_object + "/" + "range_th_max")
+                print("min:{}, max:{}".format(self.range_th_min, self.range_th_max))
+                mask_depth = np.copy(self.sub_depth_image)
+                #mask_depth[mask_depth < self.range_th_min] = 0
+                mask_depth[mask_depth < self.range_th_min] = 0
+                mask_depth_test = np.copy(mask_depth)
+                mask_depth[mask_depth <= self.range_th_max] = 255
+                mask_depth[mask_depth > self.range_th_max] = 0
+                mask_depth = mask_depth.astype(np.uint8)
 
-                equ_hist = cv2.equalizeHist(gray_img)
-                equ_img = np.hstack((gray_img, equ_hist))
-                equ_height, equ_width = equ_img.shape
-                equ_img_clip = equ_img[:, equ_width//2:equ_width]
+                debug_image_raw_msg = self.bridge.cv2_to_imgmsg(self.sub_color_image, "bgr8")
+                debug_image_raw_msg.header = self.header
+                self.pub_debug_image_raw.publish(debug_image_raw_msg)
+                
+                self.pub_debug_depth_image.publish(self.bridge.cv2_to_imgmsg(self.sub_depth_image, "16UC1"))
 
-                ret, threshold_image = cv2.threshold(equ_img_clip, 120, 255, cv2.THRESH_BINARY)
-
-                self.pub_debug_image_raw.publish(self.bridge.cv2_to_imgmsg(self.sub_image, "bgr8"))
-                if self.debug_mode == "debug":
-                    self.pub_equ_image.publish(self.bridge.cv2_to_imgmsg(equ_img_clip))
-                    self.pub_threshold_image.publish(self.bridge.cv2_to_imgmsg(threshold_image))
+                mask_depth_msg = self.bridge.cv2_to_imgmsg(mask_depth, "8UC1")
+                mask_depth_msg.header = self.header
+                
+                self.pub_mask_depth.publish(mask_depth_msg)
+                
+                self.pub_mask_depth_test.publish(self.bridge.cv2_to_imgmsg(mask_depth_test, "16UC1"))
                 
             else:
                 rospy.logwarn("not recieve image")
@@ -146,19 +111,12 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("module_name", type=str, help="module_name")
-    parser.add_argument("--debug", type=str, help="flag to launch in debug mode")
-    
+    parser.add_argument("module_name", type=str, help="module_name")  
     rospy.init_node("servo_gear", anonymous=True)
 
     args = parser.parse_args()
-
-    if args.debug == "True" or args.debug == "true":
-        print("launch in debug mode")
-        servo_gear = ServoGear(args.module_name, "debug")
-    else:
-        print("launch in normal mode")
-        servo_gear = ServoGear(args.module_name, "normal")
+    
+    servo_gear = ServoGear(args.module_name, "normal")
 
     servo_gear.run()
     

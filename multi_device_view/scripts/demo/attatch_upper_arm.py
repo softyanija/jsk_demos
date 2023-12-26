41988c0ea06a34551a232f6cc38f67af9f30024d
softyanija <amakou0614@gmail.com>
2023-12-26 19:47:05 +0900

    make grasp motion

--- multi_device_view/scripts/demo/attatch_upper_arm.py
+++ multi_device_view/scripts/demo/attatch_upper_arm.py
@@ -7,6 +7,7 @@ import tf
 import tf2_ros
 import time
 import math
+import pdb
 
 from skrobot.coordinates import CascadedCoords
 from skrobot.coordinates import Coordinates
@@ -36,12 +37,13 @@ def get_tag_coordinates(tag_name, camera_position):
             )
     except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
         rospy.logwarn("Failed to get transform")
-        pdb.set_trace()
+        base_to_tag = None
+        # pdb.set_trace()
 
-    return b2t
+    return base_to_tag
 
 
-rospy.init_node("inducdtion_screw")
+rospy.init_node("attatch_upper_arm")
 robot = skrobot.models.PR2()
 ri = PR2ROSRobotInterface(robot)
 
@@ -74,26 +76,82 @@ rarm_link_list = [
     robot.r_wrist_flex_link,
     robot.r_wrist_roll_link]
 
-init_angle_vector = array([ 0,  0,  0,  0,  0,
-                            0,  0,  0,  0,  0,
-                            0.        ,  0.        ,  0.3       ,  0.        ,  0.        ,
-                            0.87266463,  0.        , -0.9      ,  0.2       , -1.65      ,
-                            0.34906584, -1.5       , -0.17453292, -0.17453292,  0.        ,
-                            0.        ,  0.        ,  0.        ,  0.        ,  0.        ,
-                            0.        ,  0.9      ,  0.2       ,  1.65      , -0.35      ,
-                            -1.5       , -0.17453292, -0.17453292,  0.        ,  0.        ,
-                            0.        ,  0.        ,  0.        ,  0.        ,  0.        ],
-                          dtype=float32)
+init_angle_vector = np.array([ 0,  0,  0,  0,  0,
+                               0,  0,  0,  0,  0,
+                               0,  0,  0.3,  0,  0,
+                               0.87266463,  0,  -0.9,  0.2,  -1.65,
+                               0.34906584,  -1.5,  -0.17453292, -0.17453292,  0,
+                               0,  0,  0,  0,  0,
+                               0,  0.9,  0.2,  1.65,  -0.35,
+                               -1.5, -0.17453292, -0.17453292,  0,  0,
+                               0,  0,  0,  0,  0],
+                             dtype="float32")
 
+search_arm_vector = np.array([ 0,  0,  0,  0,  0,
+                               0,  0,  0,  0,  0,
+                               0,  0,  0.3,  0,  0,
+                               0.87266463,  0,  -0.9,  0.2,  -1.65,
+                               0.34906584,  -1.5,  -0.17453292, -0.17453292,  0,
+                               0,  0,  0,  0,  0,
+                               0,  0.9,  0.2,  1.65,  -0.35,
+                               -1.5, -0.17453292, -0.17453292,  0,  0,
+                               0,  0,  0,  0,  0],
+                             dtype="float32")
+tag_to_grasp_pos = skrobot.coordinates.Coordinates([-0.018, -0.000, 0.061], [-3.1, 1.0, -3.1])
+
+search_arm_rarm_vector = np.array([-0.3813113 ,  0.06809891, -2.1100695 , -1.7763098 , -1.6647813 ,
+       -0.27111033, -0.15205745], dtype="float32")
 
 robot.angle_vector(ri.angle_vector())
 robot.angle_vector(init_angle_vector)
 ri.angle_vector(robot.angle_vector(), 4)
 ri.wait_interpolation()
 
-#move position to see kxr_arm
-tag_coordinates = get_tag_coordinates("kxr_arm", "rarm")
+robot.rarm.angle_vector(search_arm_rarm_vector)
+ri.angle_vector(robot.angle_vector(), 4)
+ri.wait_interpolation()
 
+pos_counter = 0
+tag_recognition_counter = 0
+move_radius = 28
+tag_is_found = False
+
+for i in range(4):
+    for j in range(3):
+        j += 1
+        tag_coordinates = get_tag_coordinates("kxr_arm", "rarm")
+        if tag_coordinates is not None:
+            rospy.loginfo("Found arm tag")
+            tag_is_found = True
+            break
+        rospy.loginfo("Coudn't find arm tag : {} times".format(j))
+    
+    if tag_is_found:
+        break
+    else:
+        #move arm to find tag
+        rospy.loginfo("move to pos{}".format(i+1))
+        theta = math.pi/4 + i*math.pi/2
+        robot.rarm.move_end_pos([move_radius*math.cos(theta), move_radius*math.sin(theta), 0], "world")
+        ri.angle_vector(robot.angle_vector(), 4)
+        ri.wait_interpolation()
+        i += 1
+
+        if i == 4:
+            rospy.logwarn("Coudn't find arm tag in 4 pos, loop again")
+            i = 0
+
+#grasp arm
+grasp_pos = tag_coordinates.copy_worldcoords().transform(tag_to_grasp_pos)
+
+ri.move_gripper("rarm", 0.06)
+robot.inverse_kinematics(
+    grasp_pos,
+    link_list=rarm_link_list,
+    move_target=rarm_end_coords)
+ri.angle_vector(robot.angle_vector(), 4)
+ri.wait_interpolation()
+rospy.sleep(1)
 
 
 # ri.move_gripper("larm", 0.06)

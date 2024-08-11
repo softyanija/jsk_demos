9a5f763ad65b3d68410a591192c8dca7f7091666
softyanija <amakou0614@gmail.com>
2024-08-11 15:40:38 +0900

    wrote memory_line_cam2.py

--- memory_insertion/script/recognition/memory_line_cam2.py
+++ memory_insertion/script/recognition/memory_line_cam2.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+import rospy
 import cv2
-import cv_bridge
+from cv_bridge import CvBridge
 import message_filters
 import numpy as np
-import rospy
 import time
-from opencv_apps.msg import LineArrayStamped
+from opencv_apps.msg import Line,LineArrayStamped
 from geometry_msgs.msg import PoseArray
+from sensor_msgs.msg import Image
+import math
+import pdb
 
 class MemoryLineCam2():
 
@@ -18,29 +21,27 @@ class MemoryLineCam2():
         self.sub_image = None
         self.sub_memory_rect = None
         self.sub_socket_rect = None
-        self.memory_under = None
         self.result_image = None
-        self.memory_under = None
         self.memory_angle = None
-        self.socket_angle = None
+        self.memory_line = None
         self.bridge = CvBridge()
         self.pub_memory_line = rospy.Publisher('/timer_cam2_rec/memory/memory_line/line', LineArrayStamped, queue_size=1)
-        self.pub_image = rospy.Publisher('/timer_cam2_rec/memory/memory/memory_line/debug_image', Image, queue_size=1)
+        self.pub_image = rospy.Publisher('/timer_cam2_rec/memory/memory_line/debug_image', Image, queue_size=1)
         self.subscribe()
 
 
-    def subcribe(self):
+    def subscribe(self):
         sub_lines = message_filters.Subscriber('/timer_cam2_rec/memory/hough_lines/lines', LineArrayStamped)
-        sub_area = message_filters.Subscriber('/timer_cam2_rec/memory_edge', PoseArray)
+        sub_area = message_filters.Subscriber('/timer_cam2_rec/memory_edge/memory_edge', PoseArray)
         sub_image = message_filters.Subscriber('/timer_cam2/timer_cam_image/image_rect_color', Image)
-        subs = [sub_lines, sub_area, sub_image]
+        self.subs = [sub_lines, sub_area, sub_image]
         sync = message_filters.ApproximateTimeSynchronizer(fs=self.subs, queue_size=5, slop=1)
         sync.registerCallback(self.callback)
 
 
     def callback(self, lines, area, image): ##how to use data
         #for debug
-        pospy.loginfo("callback")
+        rospy.loginfo("callback")
         self.sub_memory_lines = lines.lines
         self.sub_memory_area = area.poses
         self.sub_image = self.bridge.imgmsg_to_cv2(image, "rgb8")
@@ -48,7 +49,7 @@ class MemoryLineCam2():
         # memory_line = LineArrayStamped()
 
     def run(self):
-        rate = rospy.Rate(5)
+        rate = rospy.Rate(2)
         rospy.loginfo("start memory_line_cam2")
 
         while not rospy.is_shutdown():
@@ -60,8 +61,9 @@ class MemoryLineCam2():
 
             if (self.sub_image is not None):
                 self.result_image = self.sub_image.copy()
-                
+                print()
                 if ((not self.sub_memory_lines == []) and (not self.sub_memory_area == [])):
+                    self.memory_line = LineArrayStamped()
         
                     l1 = (self.sub_memory_area[1].position.x - self.sub_memory_area[0].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[0].position.y)**2
                     l2 = (self.sub_memory_area[1].position.x - self.sub_memory_area[2].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[2].position.y)**2
@@ -74,7 +76,7 @@ class MemoryLineCam2():
                     ref_deg = math.atan(t)
                     use_line = 0
         
-                    for i,line in enumerate(self.sub_memory_lines.lines):
+                    for i,line in enumerate(self.sub_memory_lines):
                         buf = (line.pt1.y - line.pt2.y) / max(1, (line.pt1.x - line.pt2.x))
                         buf_deg = math.atan(buf)
                         diff_deg = buf_deg - ref_deg
@@ -90,17 +92,17 @@ class MemoryLineCam2():
                             line_start = (int(line.pt1.x), int(line.pt1.y))
                             line_end = (int(line.pt2.x), int(line.pt2.y))
 
-                    memory_line = Line()
+                    line = Line()
                     
                     if (abs(diff_deg_min) < 0.2):
-                        line = lines_msg.lines[use_line]
-                        memory_line.lines.append(line)
+                        line = self.sub_memory_lines[use_line]
+                        self.memory_line.lines.append(line)
                         self.result_image = cv2.line(self.result_image, line_start, line_end, (0,255,0), 3)
 
-                    self.result_image = bridge.cv2_to_imgmsg(self.result_image, "bgr8")
+                    self.result_image = self.bridge.cv2_to_imgmsg(self.result_image, "rgb8")
     
-                pub_line.publish(memory_line)
-                pub_image.publish(self.result_image)
+                self.pub_memory_line.publish(self.memory_line)
+                self.pub_image.publish(self.result_image)
 
             else:
                 rospy.logwarn("didn't recieve image")
@@ -109,5 +111,5 @@ class MemoryLineCam2():
 if __name__ == '__main__':
     rospy.init_node('memory_line_cam2')
     memory_line_cam2 = MemoryLineCam2()
-    rospy.spin()
+    memory_line_cam2.run()
 

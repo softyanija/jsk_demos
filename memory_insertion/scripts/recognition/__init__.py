b966dad8d7dc9be717823dec990e2e955b167e0a
softyanija <amakou0614@gmail.com>
2024-08-12 17:06:37 +0900

    fixed aruco_hand -> aruco_hand_pos and delete import part

--- memory_insertion/scripts/recognition/__init__.py
+++ memory_insertion/scripts/recognition/__init__.py
@@ -1,8 +1,1 @@
-__all__ = ["indicate_memory_socket_cam1", "socket_line", "memory_edge_cam2", "memory_line_cam2", "socket_cam2", "check_lever_cam2", "aruco_hand"]
-
-from . import MemorySocketCam1
-from . import SocketLineCam2
-from . import MemoryLineCam2
-from . import SocketCam2
-from . import CheckLeverCam2
-from . import ArucoHandPos
+__all__ = ["indicate_memory_socket_cam1", "socket_line", "memory_edge_cam2", "memory_line_cam2", "socket_cam2", "check_lever_cam2", "aruco_hand_pos"]

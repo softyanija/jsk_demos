d6e16d0d6127c5d0d7e3bb59fb76c231e0da40fb
softyanija <amakou0614@gmail.com>
2024-09-03 14:47:44 +0900

    change: changed to use module with built into the PC

--- memory_insertion/scripts/setup/bind_cameras.bash
+++ memory_insertion/scripts/setup/bind_cameras.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
-
 sudo rfcomm release 1
 sudo rfcomm release 2
 sleep 1
-sudo rfcomm bind 1 08:3A:F2:44:6F:EE
-sudo rfcomm bind 2 08:3A:F2:45:06:E2
+# sudo rfcomm bind 1 08:3A:F2:44:6F:EE
+# sudo rfcomm bind 2 08:3A:F2:45:06:E2
+sudo rfcomm -i hci0 bind rfcomm1 08:3A:F2:44:6F:EE 1
+sudo rfcomm -i hci0 bind rfcomm2 08:3A:F2:45:06:E2 1

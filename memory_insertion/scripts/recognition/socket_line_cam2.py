dc060f15501fa3c89af9f0c8cc82f9e72fbecaa2
softyanija <amakou0614@gmail.com>
2024-09-11 21:06:59 +0900

    add: added oneshot and loop mode in socket_line_cam2.py

--- memory_insertion/scripts/recognition/socket_line_cam2.py
+++ memory_insertion/scripts/recognition/socket_line_cam2.py
@@ -41,8 +41,39 @@ class SocketLineCam2():
         self.lines_msg = lines
         self.header = lines.header
 
+    def oneshot(self):
+        if (self.sub_image is not None):
+            self.result_image = self.sub_image.copy()
+            self.socket_line = LineArrayStamped()
+            self.socket_line.header = self.header
+            use_line = None
+                
+            if (not self.lines_msg.lines == []):
+                center_y_max = 0
+                for i,line in enumerate(self.lines_msg.lines):
+                    center_y = (line.pt1.y + line.pt2.y)/2
+                    rospy.loginfo("({}, {}, {})".format(line.pt1.y, line.pt2.y, i))
+                    if center_y > center_y_max:
+                        center_y_max = center_y
+                        use_line = i
 
-    def run(self):
+                if use_line is not None:
+                    if (2* abs(self.lines_msg.lines[use_line].pt1.y - self.lines_msg.lines[use_line].pt2.y))< abs(self.lines_msg.lines[use_line].pt1.x - self.lines_msg.lines[use_line].pt2.x) :
+                        line = self.lines_msg.lines[use_line]
+                        
+                        self.socket_line.lines.append(line)
+                        line_start = (int(line.pt1.x), int(line.pt1.y))
+                        line_end = (int(line.pt2.x), int(line.pt2.y))
+                        self.result_image = cv2.line(self.result_image, line_start, line_end, (0,255,0), 2)
+            
+            self.pub_line.publish(self.socket_line)
+            self.pub_image.publish(self.bridge.cv2_to_imgmsg(self.result_image, "bgr8"))
+
+        else:
+            rospy.logwarn("didn't recieve image")
+
+
+    def loop(self):
         rate = rospy.Rate(5)
         rospy.loginfo("start socket_line")
              
@@ -50,40 +81,13 @@ class SocketLineCam2():
             try:
                 rate.sleep()
             except rospy.ROSTimeMovedBackwardsException as e:
-                 rospy.logwarn("cought {}".format(e))
-                 pass
+                rospy.logwarn("cought {}".format(e))
+                pass
 
-            if (self.sub_image is not None):
-                self.result_image = self.sub_image.copy()
-                
-                if (not self.lines_msg.lines == []):
-                    use_line = None
-                    center_y_max = 0
-                    for i,line in enumerate(self.lines_msg.lines):
-                        center_y = (line.pt1.y + line.pt2.y)/2
-                        rospy.loginfo("({}, {}, {})".format(line.pt1.y, line.pt2.y, i))
-                        if center_y > center_y_max:
-                            center_y_max = center_y
-                            use_line = i
-
-                    if use_line is not None:
-                        if (2* abs(self.lines_msg.lines[use_line].pt1.y - self.lines_msg.lines[use_line].pt2.y))< abs(self.lines_msg.lines[use_line].pt1.x - self.lines_msg.lines[use_line].pt2.x) :
-                            line = self.lines_msg.lines[use_line]
-                            self.socket_line = LineArrayStamped()
-                            self.socket_line.header = self.header
-                            self.socket_line.lines.append(line)
-                            line_start = (int(line.pt1.x), int(line.pt1.y))
-                            line_end = (int(line.pt2.x), int(line.pt2.y))
-                            self.result_image = cv2.line(self.result_image, line_start, line_end, (0,255,0), 2)
-                            self.pub_line.publish(self.socket_line)
-
-                self.pub_image.publish(self.bridge.cv2_to_imgmsg(self.result_image, "bgr8"))
-
-            else:
-                rospy.logwarn("didn't recieve image")
+            self.oneshot()
 
 
 if __name__ == "__main__":
     rospy.init_node('socket_line_cam2')
     socket_line_cam2 = SocketLineCam2()
-    socket_line_cam2.run()
+    socket_line_cam2.loop()

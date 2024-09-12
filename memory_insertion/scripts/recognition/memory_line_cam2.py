7cde251b816797d7cbdc59716f06fb15b59c3d37
softyanija <amakou0614@gmail.com>
2024-09-12 12:21:15 +0900

    fix: Fixed bag inf memory_line_cam2.py

--- memory_insertion/scripts/recognition/memory_line_cam2.py
+++ memory_insertion/scripts/recognition/memory_line_cam2.py
@@ -39,16 +39,67 @@ class MemoryLineCam2():
         sync.registerCallback(self.callback)
 
 
-    def callback(self, lines, area, image): ##how to use data
-        #for debug
-        rospy.loginfo("callback")
+    def callback(self, lines, area, image):
         self.sub_memory_lines = lines.lines
         self.sub_memory_area = area.poses
         self.sub_image = self.bridge.imgmsg_to_cv2(image, "rgb8")
         self.header = image.header
 
 
-    def run(self):
+    def oneshot(self):
+        if (self.sub_image is not None):
+            self.result_image = self.sub_image.copy()
+            self.memory_line = LineArrayStamped()
+            rospy.loginfo(self.sub_memory_lines)
+            if ((not self.sub_memory_lines == []) and (not self.sub_memory_area == [])):
+                l1 = (self.sub_memory_area[1].position.x - self.sub_memory_area[0].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[0].position.y)**2
+                l2 = (self.sub_memory_area[1].position.x - self.sub_memory_area[2].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[2].position.y)**2
+
+                if l1 > l2:
+                    t = (self.sub_memory_area[1].position.y - self.sub_memory_area[0].position.y) / max(1, (self.sub_memory_area[1].position.x - self.sub_memory_area[0].position.x))
+                else:
+                    t = (self.sub_memory_area[1].position.y - self.sub_memory_area[2].position.y) / max(1, (self.sub_memory_area[1].position.x - self.sub_memory_area[2].position.x))
+
+                ref_deg = math.atan(t)
+                rospy.loginfo("ref_deg:{}".format(ref_deg))
+                use_line = 0
+                self.memory_angle = None
+        
+                for i,line in enumerate(self.sub_memory_lines):
+                    buf = (line.pt1.y - line.pt2.y) / max(1, (line.pt1.x - line.pt2.x))
+                    buf_deg = math.atan(buf)
+                    diff_deg = buf_deg - ref_deg
+                    rospy.loginfo("diff_deg:{} i:{}".format(diff_deg, i))
+
+                    if i == 0:
+                        diff_deg_min = diff_deg
+                        line_start = (int(line.pt1.x), int(line.pt1.y))
+                        line_end = (int(line.pt2.x), int(line.pt2.y))
+                    else:
+                        if diff_deg_min > diff_deg:
+                            diff_deg_min = diff_deg
+                            use_line = i
+                            self.memory_angle = buf_deg
+                            line_start = (int(line.pt1.x), int(line.pt1.y))
+                            line_end = (int(line.pt2.x), int(line.pt2.y))
+
+                    line = Line()
+                    
+                if (abs(diff_deg_min) < 2):
+                    line = self.sub_memory_lines[use_line]
+                    rospy.loginfo("start:({}, {}) end:({}, {})".format(line.pt1.x, line.pt1.y, line.pt2.x, line.pt2.y))
+                    self.memory_line.lines.append(line)
+                    self.result_image = cv2.line(self.result_image, line_start, line_end, (0,255,0), 3)
+
+            self.result_image = self.bridge.cv2_to_imgmsg(self.result_image, "rgb8")
+            self.pub_memory_line.publish(self.memory_line)
+            self.pub_image.publish(self.result_image)
+
+        else:
+            rospy.logwarn("didn't recieve image")
+
+
+    def loop(self):
         rate = rospy.Rate(2)
         rospy.loginfo("start memory_line_cam2")
 
@@ -59,58 +110,11 @@ class MemoryLineCam2():
                 rospy.logwarn("cought {}".format(e))
                 pass
 
-            if (self.sub_image is not None):
-                self.result_image = self.sub_image.copy()
-                if ((not self.sub_memory_lines == []) and (not self.sub_memory_area == [])):
-                    self.memory_line = LineArrayStamped()
-        
-                    l1 = (self.sub_memory_area[1].position.x - self.sub_memory_area[0].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[0].position.y)**2
-                    l2 = (self.sub_memory_area[1].position.x - self.sub_memory_area[2].position.x)**2 + (self.sub_memory_area[1].position.y - self.sub_memory_area[2].position.y)**2
-
-                    if l1 > l2:
-                        t = (self.sub_memory_area[1].position.y - self.sub_memory_area[0].position.y) / max(1, (self.sub_memory_area[1].position.x - self.sub_memory_area[0].position.x))
-                    else:
-                        t = (self.sub_memory_area[1].position.y - self.sub_memory_area[2].position.y) / max(1, (self.sub_memory_area[1].position.x - self.sub_memory_area[2].position.x))
-
-                    ref_deg = math.atan(t)
-                    use_line = 0
-                    self.memory_angle = None
-        
-                    for i,line in enumerate(self.sub_memory_lines):
-                        buf = (line.pt1.y - line.pt2.y) / max(1, (line.pt1.x - line.pt2.x))
-                        buf_deg = math.atan(buf)
-                        diff_deg = buf_deg - ref_deg
-
-                        if i == 0:
-                            diff_deg_min = diff_deg
-                            line_start = (int(line.pt1.x), int(line.pt1.y))
-                            line_end = (int(line.pt2.x), int(line.pt2.y))
-
-                        if diff_deg_min > diff_deg:
-                            diff_deg_min = diff_deg
-                            use_line = i
-                            self.memory_angle = buf_deg
-                            line_start = (int(line.pt1.x), int(line.pt1.y))
-                            line_end = (int(line.pt2.x), int(line.pt2.y))
-
-                    line = Line()
-                    
-                    if (abs(diff_deg_min) < 0.2):
-                        line = self.sub_memory_lines[use_line]
-                        self.memory_line.lines.append(line)
-                        self.result_image = cv2.line(self.result_image, line_start, line_end, (0,255,0), 3)
-
-                    self.result_image = self.bridge.cv2_to_imgmsg(self.result_image, "rgb8")
-    
-                self.pub_memory_line.publish(self.memory_line)
-                self.pub_image.publish(self.result_image)
-
-            else:
-                rospy.logwarn("didn't recieve image")
+            self.oneshot()
 
 
 if __name__ == '__main__':
     rospy.init_node('memory_line_cam2')
     memory_line_cam2 = MemoryLineCam2()
-    memory_line_cam2.run()
+    memory_line_cam2.loop()
 
